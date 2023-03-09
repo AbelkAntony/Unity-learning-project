7034a8797cd0d3af07d09290a30d8031e710fcb0
ABEL K ANTONY <abelkantony98@gmail.com>
2023-03-09 17:39:25 +0530

    now object drop in fixed time interval

--- Assets/Script/CubeSpwaner.cs
+++ Assets/Script/CubeSpwaner.cs
@@ -4,11 +4,44 @@ using UnityEngine;
 
 public class CubeSpwaner : MonoBehaviour
 {
+    public Vector3 location;
     public GameObject cube;
+    public int dropTime;
+    private float timePeriod;
+    private int randomPosition;
+    void ResetTimeToDrop()
+    {
+        timePeriod = Time.time + dropTime;
+    }
     public void CreateCube()
     {
-        Instantiate(cube, transform.position, Quaternion.identity);
+       // for(int i=1;i<=5;i++)
+      //  {
+            //Instantiate(cube, location *(i* 10) , Quaternion.identity);
+      //  }
+        //Instantiate(cube, transform.position, Quaternion.identity);
         //Debug.Log(gameObject.name);
         //Destroy(object);
+
     }
-}
+    void Start()
+    {
+        ResetTimeToDrop();
+    }
+    void Update()
+    {
+        if(Time.time > timePeriod)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                randomPosition = Random.Range(1, 3);
+                if (randomPosition == 1)
+                {
+                    Instantiate(cube, location * (i * 10), Quaternion.identity);
+                }
+                
+            }
+                ResetTimeToDrop();
+        }
+    }
+};
--- Assets/Script/planeCollition.cs
+++ Assets/Script/planeCollition.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 public class planeCollition : MonoBehaviour
 {
+    private int  numberOfCube = 0;
     public CubeSpwaner cube;
+    private int randomPosition;
+    private bool create;
     // public GameObject cube;
     void OnCollisionEnter(Collision collision)
     {
        
         //Debug.Log(collision.gameObject.name);
-        cube.CreateCube();
+       
         Destroy(collision.gameObject);
+        numberOfCube--;
         //Instantiate(cube, transform.position, Quaternion.identity);  
     }
+  /*  void Update()
+    {
+        
+        while (numberOfCube == 0)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                randomPosition = Random.Range(1, 3);
+                if (randomPosition == 1)
+                {
+                    numberOfCube++;
+                    cube.CreateCube(i);
+                }
+            }
+        }
+    }*/
 }
